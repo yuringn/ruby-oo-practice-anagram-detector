fed83dfb1c3dfcdab9348d34af62736f53cf665f
Yuri Nguyen <yuringuyen312@gmail.com>
2021-02-28 17:07:30 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require"pry"
+class Anagram
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+
+    def match(argument)
+        argument.select do |word|
+          word.split("").sort == @name.split("").sort
+        end
+    end
+
+end
+
+helo = Anagram.new("helo")
---
+++ run.rb
@@ -0,0 +1,6 @@
+require_relative "lib/anagram"
+require"pry"
+
+helo = Anagram.new("helo")
+
+binding.pry
